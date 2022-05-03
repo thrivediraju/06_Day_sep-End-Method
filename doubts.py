d6437646d9b45384dda0a41f40175a6abc193eaa
thrivediraju <104252421+thrivediraju@users.noreply.github.com>
2022-05-03 18:38:09 +0530

    Add files via upload

---
+++ doubts.py
@@ -0,0 +1,43 @@
+# print('a'+'b')
+# print('a','b')
+# print('a'+''+'b')
+# print('a'+' '+'b')
+# print('a'+'   '+'b')
+# print('a'+'-'+'b')
+
+# print(''.isspace())
+# print('n'.isspace())
+# print(' '.isspace())
+# print('    '.isspace())
+
+# print(''+'hi')
+# print(' '+'hi')
+# print('','hi')
+# print(' ','hi')
+
+# print('python',3)
+# print('python'+'3')
+# print('python'+str(3))
+# print('python'+'-'+str(3))
+
+# print(1,2,3)
+# print('1'+'-'+'2'+'-'+'3')
+
+# print(1,2,3,sep='')
+# print(1,2,3,sep=' ')
+# print(1,2,3,sep='-')
+
+# print('p'  'y')
+# print('p'                 'y')
+# print('p ' 'y')
+# print('p' ' y')
+# print('p' , 'y')
+# print('p'+' '+'y')
+# print('p','y',sep=' @ ')
+# print(10,20,30,40,sep='$ ')
+
+# print(10,20,30,40,sep='$ ',end='$')
+
+# print('hi',end='')
+# print('y',end='-')
+# print(5)
